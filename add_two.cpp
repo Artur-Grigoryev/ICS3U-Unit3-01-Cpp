873e57f31bae3aec848e7d06df736d814cbda1bc
Ubuntu <ubuntu@ip-172-31-14-26.ec2.internal>
2021-05-18 15:57:38 +0000

    fixed changes

--- add_two.cpp
+++ add_two.cpp
@@ -1,20 +1,29 @@
 // Copyright (c) 2021 Artur Grigoryev All rights reserved
 //
-// Created by Malcolm Tompkins
+// Created by Artur Grigoryev
 // Created on May 8, 2021
 // This file calculates the sum of two user input numbers
+
+
 #include <iostream>
 
-int number1 , number2, sum;
+
 int main() {
+    int number1 , number2, sum;
     // Function that calculates the sum
+    
+    // User input
     std::cout << "Input your first number: ";
     std::cin >> number1;
     std::cout << "Input your second number: ";
     std::cin >> number2;
-    // User input
-    sum = number1 + number2;
+    
     // Process
+    sum = number1 + number2;
     std::cout << "The sum of your two numbers is: ";
+    std::cout << number1;
+    std::cout << " + ";
+    std::cout << number2;
+    std::cout << " = ";
     std::cout << sum;
 }
